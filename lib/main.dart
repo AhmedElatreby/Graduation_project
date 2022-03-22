1d91b1f90aa57cb4c41d9f92a9704ee170ce982f
ahmedsport <ahmedsport@hotmail.com>
2022-03-22 14:45:28 +0000

    update1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:safetyproject/pages/signup_page.dart';
 
 import 'package:safetyproject/pages/welcome_page.dart';
 import '../pages/login_page.dart';
+import 'oauth/auth_controller.dart';
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
   runApp(const MyApp());
 }
 
