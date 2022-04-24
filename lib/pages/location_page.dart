758fe07ad9123ccb8dc6694e2f5ea6ce93800318
ahmedsport <ahmedsport@hotmail.com>
2022-04-24 20:29:52 +0100

    track current location

--- lib/pages/location_page.dart
+++ lib/pages/location_page.dart
@@ -1,12 +1,14 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sms/flutter_sms.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
-import '../location/location_service.dart';
 import '../oauth/auth_controller.dart';
-import 'package:location/location.dart';
+import '../location/mymap.dart';
 
-
+import 'package:location/location.dart' as loc;
+import 'package:permission_handler/permission_handler.dart';
 
 class LocationPage extends StatefulWidget {
   const LocationPage({Key? key}) : super(key: key);
@@ -16,15 +18,17 @@ class LocationPage extends StatefulWidget {
 }
 
 class _HomeState extends State<LocationPage> {
+  final loc.Location location = loc.Location();
+  StreamSubscription<loc.LocationData>? _locationSubscription;
+
   List<String> recipents = ["+447562596358", "+447562596358"];
-
-
-  String? lat, long, country, city, adminArea, _locationPosition;
 
   @override
   void initState() {
     super.initState();
-    getLocation();
+    _requestPermission();
+    location.changeSettings(interval: 300, accuracy: loc.LocationAccuracy.high);
+    location.enableBackgroundMode(enable: true);
   }
 
   @override
@@ -35,177 +39,182 @@ class _HomeState extends State<LocationPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('Current Location'),
+        title: const Text('Track Location'),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            GestureDetector(
-              onTap: () {
-                AuthController.instance.logOut();
-              },
-              child: Container(
-                width: width * 0.2,
-                height: height * 0.05,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  image: const DecorationImage(
-                      image: AssetImage("assests/images/loginbtn.png"),
-                      fit: BoxFit.cover),
-                ),
-                child: const Center(
-                  child: Text(
-                    "Sign out",
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+
+          GestureDetector(
+            onTap: () {
+              AuthController.instance.logOut();
+            },
+            child: Container(
+              width: width * 0.2,
+              height: height * 0.05,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: const DecorationImage(
+                    image: AssetImage("assests/images/loginbtn.png"),
+                    fit: BoxFit.cover),
+              ),
+              child: const Center(
+                child: Text(
+                  "Sign out",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
               ),
             ),
-            const SizedBox(height: 20,),
-            GestureDetector(
-              onLongPressUp: ()  {
-                String message = "$_locationPosition This is a test message!";
-                _sendSMS(message, recipents,_locationPosition);
-              },
-              child: Center(
-                child: Column(
-                  children: [
-                    ElevatedButton(
-                        onPressed: ()  {},
-                        style: ElevatedButton.styleFrom(
-                            fixedSize: const Size(150, 150),
-                            shape: const CircleBorder(),primary: Colors.redAccent
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          GestureDetector(
+            onLongPressUp: () {
+              // String message = "$_locationPosition This is a test message!";
+              // _sendSMS(message, recipents,_locationPosition);
+            },
+            child: Center(
+              child: Column(
+                children: [
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(150, 150),
+                          shape: const CircleBorder(),
+                          primary: Colors.redAccent),
+                      child: const Text(
+                        'SMS',
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
-                        child: const Text(
-                          'SMS',
-                          style: TextStyle(
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        )),
-                  ],
-                ),
+                      )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
               ),
-
             ),
-          ],
-        ),
+          ),
+
+          TextButton(
+              onPressed: () {
+                _getLocation();
+              },
+              child: Text('add my location')),
+          TextButton(
+              onPressed: () {
+                _listenLocation();
+              },
+              child: Text('enable live location')),
+          TextButton(
+              onPressed: () {
+                _stopListening();
+              },
+              child: Text('stop live location')),
+          Expanded(
+              child: StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('location').snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+              return ListView.builder(
+                  itemCount: snapshot.data?.docs.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title:
+                          Text(snapshot.data!.docs[index]['name'].toString()),
+                      subtitle: Row(
+                        children: [
+                          Text(snapshot.data!.docs[index]['latitude']
+                              .toString()),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(snapshot.data!.docs[index]['longitude']
+                              .toString()),
+                        ],
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(Icons.directions),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) =>
+                                  MyMap(snapshot.data!.docs[index].id)));
+                        },
+                      ),
+                    );
+                  });
+            },
+          )),
+        ],
       ),
     );
   }
 
-  TextStyle getStyle({double size = 20}) =>
-      TextStyle(fontSize: size, fontWeight: FontWeight.bold);
+  _getLocation() async {
+    try {
+      final loc.LocationData _locationResult = await location.getLocation();
+      await FirebaseFirestore.instance.collection('location').doc('user1').set({
+        'latitude': _locationResult.latitude,
+        'longitude': _locationResult.longitude,
+        'name': 'john'
+      }, SetOptions(merge: true));
+    } catch (e) {
+      print(e);
+    }
+  }
 
-  void getLocation() async {
-    final service = LocationService();
-    final locationData = await service.getLocation();
-
-    if(locationData != null){
-
-      final placeMark = await service.getPlaceMark(locationData: locationData);
-
+  Future<void> _listenLocation() async {
+    _locationSubscription = location.onLocationChanged.handleError((onError) {
+      print(onError);
+      _locationSubscription?.cancel();
       setState(() {
-        lat = locationData.latitude!.toStringAsFixed(2);
-        long = locationData.longitude!.toStringAsFixed(2);
-
-        country = placeMark?.country ?? 'could not get country';
-        adminArea = placeMark?.administrativeArea ?? 'could not get admin area';
+        _locationSubscription = null;
       });
+    }).listen((loc.LocationData currentlocation) async {
+      await FirebaseFirestore.instance.collection('location').doc('user1').set({
+        'latitude': currentlocation.latitude,
+        'longitude': currentlocation.longitude,
+        'name': 'john'
+      }, SetOptions(merge: true));
+    });
+  }
+
+  _stopListening() {
+    _locationSubscription?.cancel();
+    setState(() {
+      _locationSubscription = null;
+    });
+  }
+
+  _requestPermission() async {
+    var status = await Permission.location.request();
+    if (status.isGranted) {
+      print('done');
+    } else if (status.isDenied) {
+      _requestPermission();
+    } else if (status.isPermanentlyDenied) {
+      openAppSettings();
     }
   }
 }
 
-
-void _sendSMS(String message, List<String> recipents, _location ) async {
+void _sendSMS(String message, List<String> recipents) async {
   String _result = await sendSMS(message: message, recipients: recipents)
       .catchError((onError) {
     print(onError);
   });
   print(_result);
-}
-
-class LocationProvider with ChangeNotifier {
-  BitmapDescriptor? _pinLocationIcon;
-  BitmapDescriptor? get pinLocationIcon => _pinLocationIcon;
-  Map<MarkerId, Marker>? _marker;
-  Map<MarkerId, Marker>? get marker => _marker;
-
-  final MarkerId markerId = const MarkerId("1");
-
-  Location? _location;
-  Location? get location => _location;
-  LatLng? _locationPosition;
-  LatLng? get locationPosition => _locationPosition;
-
-  bool locationServiceActive = true;
-
-  LocationProvider() {
-    _location = Location();
-  }
-
-  initialization() async {
-    await getUserLocation();
-    await setCustomMapPin();
-  }
-
-  getUserLocation() async {
-    bool _serviceEnable;
-    PermissionStatus _permissionGranted;
-
-    _serviceEnable = await location!.serviceEnabled();
-    if (!_serviceEnable) {
-      _serviceEnable = await location!.requestService();
-
-      if (!_serviceEnable) {
-        return;
-      }
-    }
-
-    _permissionGranted = await location!.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await location!.requestPermission();
-
-      if (_permissionGranted != PermissionStatus.granted) {
-        return;
-      }
-    }
-
-    location!.onLocationChanged.listen((LocationData currentLocation) {
-      _locationPosition =
-          LatLng(currentLocation.latitude!, currentLocation.longitude!);
-
-      print(_locationPosition);
-
-      _marker = <MarkerId, Marker>{};
-      Marker marker = Marker(
-        markerId: markerId,
-        position: LatLng(currentLocation.latitude!, currentLocation.longitude!),
-        icon: (pinLocationIcon)!,
-        draggable: true,
-        onDragEnd: ((newPosition) {
-          _locationPosition =
-              LatLng(newPosition.latitude, newPosition.longitude);
-          notifyListeners();
-        }),
-      );
-
-      _marker![markerId] = marker;
-      notifyListeners();
-    });
-  }
-
-  setCustomMapPin() async {
-    _pinLocationIcon = await BitmapDescriptor.fromAssetImage(
-      const ImageConfiguration(devicePixelRatio: 2.5),
-      'assets/pin1.png',
-    );
-  }
 }
